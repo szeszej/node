ff84a391419e979d4444a26a14e5614379048165
unknown <>
2021-02-09 11:02:31 +0100

    more weather app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,66 +1,66 @@
-const yargs = require('yargs')
-const notes = require('./notes.js')
+const yargs = require("yargs");
+const notes = require("./notes.js");
 
 //Add command
 yargs.command({
-  command: 'add',
-  describe: 'Add a new note',
+  command: "add",
+  describe: "Add a new note",
   builder: {
     title: {
-      describe: 'Note title',
+      describe: "Note title",
       demandOption: true,
-      type: 'string',
+      type: "string",
     },
     body: {
-      describe: 'Note description',
+      describe: "Note description",
       demandOption: true,
-      type: 'string',
+      type: "string",
     },
   },
   handler(argv) {
-    notes.addNote(argv.title, argv.body)
+    notes.addNote(argv.title, argv.body);
   },
-})
+});
 
 //Remove command
 yargs.command({
-  command: 'remove',
-  describe: 'Remove a note',
+  command: "remove",
+  describe: "Remove a note",
   builder: {
     title: {
-      describe: 'The title of note to be removed',
-      type: 'string',
+      describe: "The title of note to be removed",
+      type: "string",
       demandOption: true,
     },
   },
   handler(argv) {
-    notes.removeNote(argv.title)
+    notes.removeNote(argv.title);
   },
-})
+});
 
 //List command
 yargs.command({
-  command: 'list',
-  describe: 'List notes',
+  command: "list",
+  describe: "List notes",
   handler() {
-    notes.listNotes()
+    notes.listNotes();
   },
-})
+});
 
 //Read command
 yargs.command({
-  command: 'read',
-  describe: 'Read note',
+  command: "read",
+  describe: "Read note",
   builder: {
     title: {
-      describe: 'Title of the note to read',
-      type: 'string',
+      describe: "Title of the note to read",
+      type: "string",
       demandOption: true,
     },
   },
   handler(argv) {
-    notes.readNote(argv.title)
+    notes.readNote(argv.title);
   },
-})
+});
 
-yargs.parse()
+yargs.parse();
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,9 @@
-console.log('Starting')
+const request = require("request");
 
-setTimeout(() => {
-  console.log('czekamy 2 sekundy')
-}, 2000)
+const url =
+  "http://api.weatherstack.com/current?access_key=ba9d10fcb561e8f08bac12df4da64d40&query=37.8267,-122.4233";
 
-setTimeout(() => {
-  console.log('czekamy 0 sekund')
-}, 0)
-
-console.log('Stopping')
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
