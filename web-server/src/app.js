bf5f70d11455143ac39ebc46166dad38c4bfe690
unknown <>
2021-02-10 10:44:28 +0100

    some more work

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,25 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello express!");
+  res.send("<h1>Weather</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Rohan requires aid!");
+  res.send([
+    {
+      name: "Krzysiek",
+      age: 34,
+    },
+    { name: "Sarah" },
+  ]);
 });
 
 app.get("/about", (req, res) => {
-  res.send("It's all about us!");
+  res.send("<h1>It's all about us!</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Current weather for you convenience!");
+  res.send({ location: "Świdnik", forecast: "It's raining men!" });
 });
 
 app.listen(5000, () => {
