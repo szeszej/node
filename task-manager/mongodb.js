d31add3b2be055ab8a727e2cc92e77fcd482d42e
szeszej <szeszej@gmail.com>
2021-02-13 18:32:33 +0100

    yet another lesson

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,9 +1,14 @@
-const mongodb = require('mongodb')
+// const mongodb = require('mongodb')
 
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(
   connectionURL,
@@ -13,18 +18,20 @@ MongoClient.connect(
       return console.log('Unable to connect to database!')
     }
     const db = client.db(databaseName)
-    // db.collection('users').insertOne(
-    //   {
-    //     name: 'szeszej',
-    //     age: 34,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log('Unable to insert user')
-    //     }
-    //     console.log(result.ops)
-    //   },
-    // )
+
+    db.collection('users').insertOne(
+      {
+        _id: id,
+        name: 'Karolajna',
+        age: 33,
+      },
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert user')
+        }
+        console.log(result.ops)
+      },
+    )
     // db.collection('users').insertMany(
     //   [
     //     { name: 'Olga', age: 33 },
@@ -37,18 +44,18 @@ MongoClient.connect(
     //     console.log(result.ops)
     //   },
     // )
-    db.collection('tasks').insertMany(
-      [
-        { description: 'Pranie', completed: false },
-        { description: 'Zmywanie', completed: true },
-        { description: 'Sprzątanie', completed: false },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log('Unable to insert tasks!')
-        }
-        console.log(result.ops)
-      },
-    )
+    // db.collection('tasks').insertMany(
+    //   [
+    //     { description: 'Pranie', completed: false },
+    //     { description: 'Zmywanie', completed: true },
+    //     { description: 'Sprzątanie', completed: false },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert tasks!')
+    //     }
+    //     console.log(result.ops)
+    //   },
+    // )
   },
 )
