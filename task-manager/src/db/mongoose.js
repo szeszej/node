8d5f751996f916f1dd642c6d30c4498305fa6bef
unknown <>
2021-02-20 14:25:02 +0100

    models

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,21 +1,77 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
-})
+});
 
-const User = mongoose.model('User', {
-  name: {
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error("Invalid age!");
+//       }
+//     },
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     minlength: 7,
+//     validate(value) {
+//       if (value.toLowerCase().includes("password")) {
+//         throw new Error("Invalid password!");
+//       }
+//     },
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error("Invalid email");
+//       }
+//     },
+//   },
+// });
+
+// const me = new User({
+//   name: "   szeszej   ",
+//   email: "szeSZej@szeszej.com",
+//   password: "password",
+// });
+
+// me.save()
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log("Error", error.errors));
+
+const Task = mongoose.model("Task", {
+  description: {
     type: String,
+    required: true,
+    trim: true,
   },
-  age: {
-    type: Number,
+  completed: {
+    type: Boolean,
+    default: false,
   },
-})
+});
 
-const me = new User({ name: 'szeszej', age: 'mike' })
+const learningNode = new Task({
+  description: "  Prezka na konfe    ",
+});
 
-me.save()
+learningNode
+  .save()
   .then((result) => console.log(result))
-  .catch((error) => console.log('Error', error))
+  .catch((result) => console.log(result));
