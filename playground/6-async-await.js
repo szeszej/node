763d82cc9da27bf8bfc94c4ac2b8888ff7687f31
unknown <>
2021-02-27 11:50:18 +0100

    async await

--- playground/6-async-await.js
+++ playground/6-async-await.js
@@ -1,5 +1,18 @@
-const doWork = async () => {
-  return 'lalala'
-}
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
 
-doWork().then((res) => console.log(res))
+const doWork = async () => {
+  const sum = await add(1, 2);
+  const sum2 = await add(sum, 3);
+  const sum3 = await add(sum2, 4);
+  return sum3;
+};
+
+doWork()
+  .then((res) => console.log(res))
+  .catch((e) => console.log(e));
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,10 +1,12 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-User.findOneAndUpdate({ _id: "6032973903f8321b8cd97cda" }, { age: 3 })
-  .then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 3 });
-  })
-  .then((result) => console.log(result))
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findOneAndUpdate({ _id: id }, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("6032973903f8321b8cd97cda", 5)
+  .then((res) => console.log(res))
   .catch((e) => console.log(e));
